bea1afd5e15fef68aa2792ee1c7215c8bc826b4c
istrav <travis.burandt@gmail.com>
2021-05-07 00:54:42 -0500

    blocks 2

--- api/v1/pages.js
+++ api/v1/pages.js
@@ -100,3 +100,20 @@ export function remove (eventSource) {
     })
 }
 
+export function blocks (eventSource) {
+  let url = `${host}/${version}/${endpoint}/blocks`
+  eventSource.url = url
+  return axios
+    .post(url, {
+      params: eventSource
+    })
+    .then(function (response) {
+      response.data.clientAt = Date.now()
+      // console.log(`REST ::: ${JSON.stringify(response.data, null, 2)}`)
+      return response.data
+    })
+    .catch(function (error) {
+      // console.log(`REST ::: ${JSON.stringify(error, null, 2)}`)
+      return error
+    })
+}
